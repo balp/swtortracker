d2182836e4d1e0a050f983a160b49d7e622852f7
Anders Arnholm <anders@arnholm.se>
2016-02-14 13:26:05 +0100

    Split addform.js

--- client/main.js
+++ client/main.js
@@ -11,24 +11,3 @@ Template.charaterList.helpers({
     return Charaters.find( {owner: Meteor.userId() } );
   }
 });
-
-Template.addCharacter.events({
-  'submit form': function(e, b) {
-    console.log( $("#charServer" ) )
-    var newChar = {
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-      server: $('#charServer').val(),
-      allegiance: $('#charAllegiance').val(),
-      name: $('#charName').val(),
-      species: $('#charSpecies').val(),
-      baseClass: $('#charClass').val(),
-      advClass: $('#charAdvClass').val(),
-      skillTree: $('#charSkillTree').val()
-    };
-    console.log(newChar)
-    Meteor.call("addCharacter", newChar);
-    //Charaters.insert(newChar)
-    $('#addCharacterForm').find('input:text').val('');
-  }
-});
